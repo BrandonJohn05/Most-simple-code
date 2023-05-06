80adb244585bfc65505173c762c515aa57f60cf5
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2023-05-05 19:25:45 -0600

    14 billion years to run this

---
+++ UniverseAge.cpp
@@ -0,0 +1,7 @@
+//Runs for approximately 14 billion years on the average device.
+
+int main()
+{	for(int chain[27]= {0}; chain[0] != 4; chain[26]++)
+	{	for(int a = 26; chain[a] == 10; a--) {chain[a] = 0; chain[a - 1]++;}
+	}
+}
